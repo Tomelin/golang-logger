48c9443edc930f5a39ca2a4330f7c1b37f4b1320
Rafael Tomelin <rafael.tomelin@groupsantander.com>
2021-08-01 02:57:23 -0300

    ADD logger

---
+++ main.go
@@ -0,0 +1,35 @@
+package golanglogger
+
+import (
+	"log"
+	"time"
+)
+
+var loggerTime = time.Now()
+
+func main() {
+
+	return
+}
+
+func Kafka() time.Time {
+
+	return loggerTime
+}
+
+func Cache() {
+	return
+}
+
+// Loga as URI chamadas
+func Logger() {
+	log.Println("Logger", loggerTime)
+	return
+}
+
+// Sample
+// if rota.RequerAutenticacao {
+// 	r.HandleFunc(rota.URI, middlewares.Logger(middlewares.Autenticar(rota.Funcao))).Methods(rota.Metodo)
+// } else {
+// 	r.HandleFunc(rota.URI, middlewares.Logger(rota.Funcao)).Methods(rota.Metodo)
+// }
