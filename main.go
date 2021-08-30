f706e59fbb0dc333f55869c9c24847a215fb8685
Rafael Tomelin <rafael.tomelin@groupsantander.com>
2021-08-30 17:25:07 -0300

    add code

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package golanglogger
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 	"time"
 )
 
 var loggerTime = time.Now()
+
+type LoggerErr struct {
+	APIName    string
+	file       string
+	err        error
+	statusCode *string
+	time       time.Time
+}
 
 func main() {
 
@@ -27,9 +37,9 @@ func Cache() {
 	return
 }
 
-// Loga as URI chamadas
-func Logger() {
-	log.Println("Logger", loggerTime)
+// Loga os erros da API.  Parametros necessários APINmae, nome do arquivo, erro, status code
+func Logger(APIName string, file string, err error, statusCode string) {
+	log.Println("Logger....", APIName, file, err, statusCode, time.Now())
 	return
 }
 
@@ -81,4 +91,63 @@ func FileName(fName string) {
 	fmt.Println("File => ", filename)
 
 	return
+}
+
+func LoggerFile(fName string) {
+	filename := filepath.Base(&fName)
+
+	pwd, err := filepath.Dir(&fName)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	ss := strings.Split(pwd, "/")
+	pathFolder := ss[len(ss)-1]
+
+	fmt.Println("Folder =>", pathFolder)
+
+	fmt.Println("File => ", filename)
+	err := filepath.Walk(filepath.Dir(&fName), getModuleName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+
+}
+
+func getModuleName(path string, info os.FileInfo, err error) string {
+
+	if err != nil {
+		return nil
+	}
+
+	pathSplit := strings.Split(path, "/")
+	fName := pathSplit[len(pathSplit)-1]
+	if fName == "go.mod" {
+		oFile, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer oFile.Close()
+
+		scanner := bufio.NewScanner(oFile)
+		r, err := regexp.Compile("module")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for scanner.Scan() {
+			if r.MatchString(scanner.Text()) {
+				oSplit := strings.Split(scanner.Text(), " ")
+				return oSplit[1]
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return nil
 }
